3483fc3288095552c0b4af0b9f5883d48925b267
Alex Aleksandrov <44984255+AlexLite@users.noreply.github.com>
2026-07-15 13:58:53 +0400

    test(db): remove temporary alembic head probe

--- apps/api/tests/test_alembic_head_probe.py
+++
@@ -1,9 +0,0 @@
-from alembic.config import Config
-from alembic.script import ScriptDirectory
-
-
-def test_report_current_alembic_head():
-    config = Config("alembic.ini")
-    script = ScriptDirectory.from_config(config)
-    heads = script.get_heads()
-    assert False, f"ALEMBIC_HEADS={heads}"
